107c3277ca105a984d995dd69d9bf6a083434734
timislearning <tim.mitzen@gmail.com>
2020-08-11 18:33:15 -0700

    day 1

--- src/adv.py
+++ src/adv.py
@@ -2,46 +2,53 @@ from room import Room
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+outside = Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons")
+foyer = Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east.""")
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+overlook = Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""")
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+narrow =  Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""")
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+treasure = Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+earlier adventurers. The only exit is to the south.""")
+
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+outside.n_to = foyer
+foyer_to = outside
+foyer.n_to = overlook
+foyer.e_to = narrow
+overlook.s_to = foyer
+narrow.w_to = foyer
+narrow.n_to = treasure
+treasure.s_to = narrow
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+new_player_object = room
+user_input = input('Please input a direction to go. For north use `n`, For south use `s`, for east `e` and for west use `w`'
+                   'to quit hit `q` ')
 
 # Write a loop that:
+while
 #
 # * Prints the current room name
+if room == 'foyer' :
+    print('You are in the foyer')
+if room == 'treasure':
+    print('You are in the treasure room')
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
